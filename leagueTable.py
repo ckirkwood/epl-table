6636888b1acfc1e468803d66c847f8501839564e
Callum Kirkwood <CallumKirkwood@Toms-MacBook-Air.local>
2017-07-16 22:35:23 +0100

    fix port

--- leagueTable.py
+++ leagueTable.py
@@ -187,4 +187,6 @@ def not_found(error):
 
 # Main programme logic
 if __name__ == "__main__":
-	app.run(host='::', port=80, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 33507))
+	app.run(host='::', port=port, debug=True)
