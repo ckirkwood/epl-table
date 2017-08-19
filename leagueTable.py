4a9800c572afa01a5f4a69826492765699188407
Callum Kirkwood <callum.kirkwood@gmail.com>
2017-08-19 16:34:35 +0100

    add more fields

--- leagueTable.py
+++ leagueTable.py
@@ -106,6 +106,21 @@ def get_data():
 def team_at_position(pos):
     return(data['standing'][pos]['teamName'])
 
+def played(pos):
+    return(data['standing'][pos]['playedGames'])
+
+def won(pos):
+    return(data['standing'][pos]['wins'])
+
+def lost(pos):
+    return(data['standing'][pos]['losses'])
+
+def draws(pos):
+    return(data['standing'][pos]['draws'])
+
+def goalDifference(pos):
+    return(data['standing'][pos]['goalDifference'])
+
 def team_points(pos):
     return(data['standing'][pos]['points'])
 
@@ -115,40 +130,38 @@ def player_name(pos):
 # set up arrays for table columns
 position = range(1, 21)
 team = []
+gamesPlayed = []
+w = []
+l = []
+d = []
+gd = []
 points = []
 
 #populate blank arrays
 for i in range(20):
     team.append(team_at_position(i))
+    gamesPlayed.append(played(i))
+    w.append(won(i))
+    l.append(lost(i))
+    d.append(draws(i))
+    gd.append(goalDifference(i))
     points.append(team_points(i))
 
 # create table
-t = PrettyTable(['#', 'Team', 'Player', 'Pts'])
-t.add_row([position[0], team[0], player_name(0), points[0]])
-t.add_row([position[1], team[1], player_name(1), points[1]])
-t.add_row([position[2], team[2], player_name(2), points[2]])
-t.add_row([position[3], team[3], player_name(3), points[3]])
-t.add_row([position[4], team[4], player_name(4), points[4]])
-t.add_row([position[5], team[5], player_name(5), points[5]])
-t.add_row([position[6], team[6], player_name(6), points[6]])
-t.add_row([position[7], team[7], player_name(7), points[7]])
-t.add_row([position[8], team[8], player_name(8), points[8]])
-t.add_row([position[9], team[9], player_name(9), points[9]])
-t.add_row([position[10], team[10], player_name(10), points[10]])
-t.add_row([position[11], team[11], player_name(11), points[11]])
-t.add_row([position[12], team[12], player_name(12), points[12]])
-t.add_row([position[13], team[13], player_name(13), points[13]])
-t.add_row([position[14], team[14], player_name(14), points[14]])
-t.add_row([position[15], team[15], player_name(15), points[15]])
-t.add_row([position[16], team[16], player_name(16), points[16]])
-t.add_row([position[17], team[17], player_name(17), points[17]])
-t.add_row([position[18], team[18], player_name(18), points[18]])
-t.add_row([position[19], team[19], player_name(19), points[19]])
+t = PrettyTable(['#', 'Team', 'Player', 'Pld', 'W', 'L', 'D', 'GD', 'Pts'])
+
+for i in range(20):
+    t.add_row([position[i], team[i], player_name(i), gamesPlayed[i], w[i], l[i], d[i], gd[i], points[i]])
 
 # print table to console
 t.align["#"] = "c"
 t.align["Team"] = "l"
 t.align["Player"] = "l"
+t.align["Pld"] = "r"
+t.align["W"] = "r"
+t.align["L"] = "r"
+t.align["D"] = "r"
+t.align["GD"] = "r"
 t.align["Pts"] = "r"
 t.format = False
 print(t)
@@ -175,8 +188,6 @@ def export_html(element, url, body):
 	</head>
 	<body>
     <p>url: <a href=\"%s\">%s</a></p><p>%s</p>
-    <div id="button1"><a href="http://apps.callumkirkwood.com/epl-table/api/v1.0/">Back</a></div>
-	<div id="button2"><a href="http://apps.callumkirkwood.com/epl-table/api/v1.0/data">Data</a></div>
 	</body>
 	</html>
 	"""
